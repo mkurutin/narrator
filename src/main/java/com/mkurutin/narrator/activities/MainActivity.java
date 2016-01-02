5e4aef44f788e7048e33be1144a1ef96692a8c99
Mykola Kurutin <nickolai.kurutin@gmail.com>
2016-01-02 18:30:23 -0500

    Open FilePicker on Select file menu option

--- src/main/java/com/mkurutin/narrator/activities/MainActivity.java
+++ src/main/java/com/mkurutin/narrator/activities/MainActivity.java
@@ -1,11 +1,15 @@
 package com.mkurutin.narrator.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import com.devpaul.filepickerlibrary.FilePickerActivity;
+import com.devpaul.filepickerlibrary.enums.FileScopeType;
 import com.mkurutin.narrator.R;
 import com.mkurutin.narrator.config.NarratorApplication;
 import com.mkurutin.narrator.services.AudioPlayer;
@@ -38,10 +42,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void playAudio(View button) {
-//        Intent filePickerIntent = new Intent(this, FilePickerActivity.class);
-//        filePickerIntent.putExtra(FilePickerActivity.SCOPE_TYPE, FileScopeType.ALL);
-//        filePickerIntent.putExtra(FilePickerActivity.REQUEST_CODE, FilePickerActivity.REQUEST_FILE);
-//        startActivityForResult(filePickerIntent, FilePickerActivity.REQUEST_FILE);
         audioPlayer.startFromLastPosition();
     }
 
@@ -80,5 +80,17 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.app_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_select_file) {
+            Intent filePickerIntent = new Intent(this, FilePickerActivity.class);
+            filePickerIntent.putExtra(FilePickerActivity.SCOPE_TYPE, FileScopeType.ALL);
+            filePickerIntent.putExtra(FilePickerActivity.REQUEST_CODE, FilePickerActivity.REQUEST_FILE);
+            startActivityForResult(filePickerIntent, FilePickerActivity.REQUEST_FILE);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
